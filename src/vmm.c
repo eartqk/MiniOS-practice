632c81a6348361b1e19cb4109598ab06f15fa0b7
eartqk <eartqk@gmail.com>
2021-07-06 19:01:54 +0300

    Fixed.

--- src/vmm.c
+++ src/vmm.c
@@ -1,6 +1,7 @@
 #include "vmm.h"
 
-#include "idt.h"
+#include "descriptor_tables.h"
+#include "isr.h"
 #include "pmm.h"
 
 u32int *page_directory = (u32int *)PAGE_DIR_VIRTUAL_ADDR;
@@ -116,7 +117,6 @@ void page_fault(registers_t *regs) {
 
 	printk("Page fault at 0x%x, faulting address 0x%x\n", regs->eip, cr2);
 	printk("Error code: %x\n", regs->err_code);
-	panic("");
 	for (;;)
 		;
 }
