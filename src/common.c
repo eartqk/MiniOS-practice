c2a674eae81dd3b557e669fabc4ddbfc006e0403
eartqk <eartqk@gmail.com>
2021-07-06 19:06:37 +0300

    Corrected panic.

--- src/common.c
+++ src/common.c
@@ -96,20 +96,28 @@ int strlen(char *src)
     return i;
 }
 
-extern void panic(const char *message, const char *file, u32int line)
-{
-    // We encountered a massive problem and have to stop.
-    asm volatile("cli"); // Disable interrupts.
+// extern void panic(const char *message, const char *file, u32int line)
+// {
+//     // We encountered a massive problem and have to stop.
+//     asm volatile("cli"); // Disable interrupts.
 
-    monitor_write("PANIC(");
-    monitor_write(message);
-    monitor_write(") at ");
-    monitor_write(file);
-    monitor_write(":");
-    monitor_write_dec(line);
-    monitor_write("\n");
-    // Halt by going into an infinite loop.
-    for(;;);
+//     monitor_write("PANIC(");
+//     monitor_write(message);
+//     monitor_write(") at ");
+//     monitor_write(file);
+//     monitor_write(":");
+//     monitor_write_dec(line);
+//     monitor_write("\n");
+//     // Halt by going into an infinite loop.
+//     for(;;);
+// }
+
+extern void panic(const char *msg)
+{
+  printk ("*** System panic: %s\n", msg);
+  print_stack_trace ();
+  printk ("***\n");
+  for (;;) ;
 }
 
 extern void panic_assert(const char *file, u32int line, const char *desc)
