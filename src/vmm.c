10d26775401fcf3746c68714a5f659abc5ebeafd
eartqk <eartqk@gmail.com>
2021-07-06 19:07:35 +0300

    Panic added back.

--- src/vmm.c
+++ src/vmm.c
@@ -117,6 +117,7 @@ void page_fault(registers_t *regs) {
 
 	printk("Page fault at 0x%x, faulting address 0x%x\n", regs->eip, cr2);
 	printk("Error code: %x\n", regs->err_code);
+	panic("Error.");
 	for (;;)
 		;
 }
